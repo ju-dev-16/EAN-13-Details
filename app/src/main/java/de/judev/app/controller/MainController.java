9e1a0754e5f79f0b49525847aaed6233a399b3e2
JuDev <jahidudd65@gmail.com>
2022-08-08 19:13:11 +0200

    Add barcode validation feedback on the ui

--- app/src/main/java/de/judev/app/controller/MainController.java
+++ app/src/main/java/de/judev/app/controller/MainController.java
@@ -13,16 +13,28 @@ import de.judev.app.service.BarcodeValidatorService;
 public class MainController {
     
     private UserInput userInput = new UserInput();
+    private String isValid;
+    private String icon;
+    private String fill;
 
     private BarcodeValidatorService service = new BarcodeValidatorService();
+
+    public MainController() {
+
+        this.isValid = "";
+        this.icon = "";
+        this.fill = "";
+    }
 
     @GetMapping("/")
     public String showHome(Model model) {
 
-        model.addAttribute("USER_INPUT", userInput);
-        model.addAttribute("BARCODE", userInput.getBarcode());
+        model.addAttribute("USER_INPUT", this.userInput);
+        model.addAttribute("BARCODE", this.userInput.getBarcode());
 
-        model.addAttribute("ISVALID", "");
+        model.addAttribute("ISVALID", this.isValid);
+        model.addAttribute("ICON", this.icon);
+        model.addAttribute("FILL", this.fill);
 
         return "home";
     }
@@ -32,16 +44,17 @@ public class MainController {
 
         this.userInput = userInput;
 
-        model.addAttribute("USER_INPUT", this.userInput);
-        model.addAttribute("BARCODE", this.userInput.getBarcode());
-
         if (service.validateBarcode(this.userInput.getBarcode()).isValid()) {
 
-            model.addAttribute("ISVALID", "is valid.");
+            this.isValid = "is valid.";
+            this.icon = "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z";
+            this.fill = "lightgreen";
 
         } else {
 
-            model.addAttribute("ISVALID", "is not valid.");
+            this.isValid = "is unvalid.";
+            this.icon = "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z";
+            this.fill = "red";
 
         }
 
